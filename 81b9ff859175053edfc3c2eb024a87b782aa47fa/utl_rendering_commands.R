879d1142542e667adee0e71b2f33c699426a8ba9
dom <liebl.dominik@gmail.com>
2024-04-03 13:17:19 +0200

    upd

---
+++ 81b9ff859175053edfc3c2eb024a87b782aa47fa/utl_rendering_commands.R
@@ -0,0 +1,12 @@
+## From within R
+
+library("quarto")
+quarto::quarto_render(
+    input         = "index.qmd", 
+    cache_refresh = TRUE, 
+    output_format = "html") # defaults to html
+
+## From terminal
+quarto render --cache-refresh
+
+# pwd: compstat
